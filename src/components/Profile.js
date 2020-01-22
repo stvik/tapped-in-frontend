ff5778f7ecb4443ed564268c324bac82e385806b
stvik <stvik2@gmail.com>
2020-01-22 11:18:59 -0500

    favorites to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 import {Grid, Image, Header} from 'semantic-ui-react'
+import BreweriesListContainer from '../containers/BreweriesListContainer'
 
 
 class Profile extends Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            favoriteBrews: [],
+            selectedBrewery: null
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:3000/user_brews?user_id=${this.props.user}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const breweries = data.map(fav => fav.brewery)
+            this.setState({favoriteBrews: breweries})
+
+        })
+
+    }
+
+    selectedBrewery = (brewery) => {
+        this.setState({selectedBrewery: brewery})
+    }
+
+
+
     render(){
+        console.log(this.state.favoriteBrews)
         const {firstname, lastname, image, bio} = this.props.user
         return(
             <Grid >
@@ -23,6 +52,7 @@ class Profile extends Component {
                 </Grid.Column>
                 <Grid.Column width={10}>
                 <Header size='large'>Favorite Breweries</Header>
+                {this.state.favoriteBrews.length ? <BreweriesListContainer user={this.props.user} selectedBrewery={this.selectedBrewery} breweries={this.state.favoriteBrews} /> : null}
                 </Grid.Column>
                 </Grid.Row>
         </Grid>
--- src/containers/BreweriesListContainer.js
+++ src/containers/BreweriesListContainer.js
@@ -5,16 +5,9 @@ import {Item, Segment} from 'semantic-ui-react'
 
 export default class BreweriesListContainer extends React.Component {
 
-	// decideDisplay = () => {
-	// 	if (this.props.breweries.length) {
-	// 		return this.props.breweries.map((brewery,index) => <BreweryCard brewery={brewery} key={index}/>)
-	// 	} else if (this.props.page > 1) {
-	// 		return <div>No breweries to display</div>
-	// 	}
-	// }
+
 
 	render() {
-		// console.log(this.props.breweries.length)
 		
 		return (
 
